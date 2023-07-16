38369b1186359d1b04e517c2bf3d3d7b0fda0cc3
Andrej <andrejtrajkovskimanich@gmail.com>
2023-07-16 17:13:29 +0200

    making a model window

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const closeButton = document.querySelector(`.close-modal`);
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const openBtn = document.querySelectorAll(`.show-modal`);
+
+const open = function(){
+    modal.classList.remove(`hidden`);
+    overlay.classList.remove(`hidden`);
+    }
+
+const close = function(){
+    modal.classList.add(`hidden`);
+    overlay.classList.add(`hidden`);
+}
+
+for( let i = 0;i<openBtn.length;i++){
+openBtn[i].addEventListener(`click`,open)
+}
+
+closeButton.addEventListener(`click`,close)
+overlay.addEventListener(`click`,close)
+
+document.addEventListener(`keydown`,function(e){
+    if(e.key === `Escape` && !modal.classList.contains(`hidden`)){
+        close()
+    }
+})
+
+
