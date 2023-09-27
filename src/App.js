47f021edef7fa791e5c260b70ab6e149e5c256a3
Andrej <andrejtrajkovskimanich@gmail.com>
2023-09-28 00:39:04 +0200

    Bez BackEnd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper.tsx';
+
+function App() {
+  return (
+    <div className="App">
+    <TodoWrapper></TodoWrapper>
+    </div>
+  );
+}
+
+export default App;
